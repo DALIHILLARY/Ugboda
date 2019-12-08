66cc01a712920b0ec4c2499e6e5f5add33fb701a
DALI HILLARY <dalirichardh@gmail.com>
2019-12-08 23:26:48 +0300

    fixing off some bugs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,10 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\MemberUpload::class,
-        Commands\EnrolNum::class,
-        Commands\AgentNum::class,
-        Commands\PaymentUpdate::class,
+          //
     ];
 
     /**
